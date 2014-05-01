69acce5c48e5aab5616483c84c418691f8e36a7f
witspirit <witspirit@gmail.com>
2014-05-01 23:24:22 +0200

    Upgraded project to Java 8 Used Java 8 streams to reduce code duplication in SumAssert

--- sums-core/src/test/java/be/witspirit/mathex/support/SumAssert.java
+++ sums-core/src/test/java/be/witspirit/mathex/support/SumAssert.java
@@ -5,6 +5,7 @@ import be.witspirit.mathex.Sum3;
 import org.junit.Assert;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Helpful assertions on Sums
@@ -15,81 +16,13 @@ public class SumAssert {
 
 
     public static void assertCompliantAndDiverseSums(int size, int min, int max, List<Sum> sums) {
-        if (size < 10) {
-            throw new IllegalArgumentException("Minimum sample size = 10");
-        }
-        if (max - min < 3) {
-            throw new IllegalArgumentException("Minimum difference between min and max = 3");
-        }
-        Assert.assertNotNull(sums);
-        Assert.assertEquals(size, sums.size());
-
-        Set<Integer> uniqueInput1 = new HashSet<>();
-        Set<String> uniqueOperator = new HashSet<>();
-        Set<Integer> uniqueInput2 = new HashSet<>();
-        Set<Integer> uniqueOutput = new HashSet<>();
-
-        for (Sum sum : sums) {
-            System.out.println(sum.toString());
-
-            Assert.assertTrue(sum.getInput1() >= min && sum.getInput1() <= max);
-            Assert.assertTrue(sum.getOperator().equals("+") || sum.getOperator().equals("-"));
-            Assert.assertTrue(sum.getInput2() >= min && sum.getInput2() <= max);
-            Assert.assertTrue(sum.getOutput() >= min && sum.getOutput() <= max);
-
-            uniqueInput1.add(sum.getInput1());
-            uniqueOperator.add(sum.getOperator());
-            uniqueInput2.add(sum.getInput2());
-            uniqueOutput.add(sum.getOutput());
-
-        }
-
-        Set<Sum> uniqueSums = new HashSet<>(sums);
-        Assert.assertTrue(uniqueSums.size()>1);
-
-        Assert.assertTrue("Insufficient variation on input1", uniqueInput1.size()>1);
-        Assert.assertTrue("Insufficient variation on operator", uniqueOperator.size()>1);
-        Assert.assertTrue("Insufficient variation on input2", uniqueInput2.size()>1);
-        Assert.assertTrue("Insufficient variation on output", uniqueOutput.size()>1);
+        List<SumRep> sumReps = sums.stream().map(sum -> new SumRepAdapter(sum)).collect(Collectors.toList());
+        assertCompliantAndDiverseSumReps(size, min, max, true, sumReps);
     }
 
     public static void assertCompliantAndDiverseSum3s(int size, int min, int max, List<Sum3> sums) {
-        if (size < 10) {
-            throw new IllegalArgumentException("Minimum sample size = 10");
-        }
-        if (max - min < 3) {
-            throw new IllegalArgumentException("Minimum difference between min and max = 3");
-        }
-        Assert.assertNotNull(sums);
-        Assert.assertEquals(size, sums.size());
-
-        Set<Integer> uniqueInput1 = new HashSet<>();
-        Set<Integer> uniqueInput2 = new HashSet<>();
-        Set<Integer> uniqueInput3 = new HashSet<>();
-        Set<Integer> uniqueOutput = new HashSet<>();
-
-        for (Sum3 sum : sums) {
-            System.out.println(sum.toString());
-
-            Assert.assertTrue(sum.getT1() >= min && sum.getT1() <= max);
-            Assert.assertTrue(sum.getT2() >= min && sum.getT2() <= max);
-            Assert.assertTrue(sum.getT3() >= min && sum.getT3() <= max);
-            Assert.assertTrue(sum.getOutput() >= min && sum.getOutput() <= max);
-
-            uniqueInput1.add(sum.getT1());
-            uniqueInput2.add(sum.getT2());
-            uniqueInput3.add(sum.getT3());
-            uniqueOutput.add(sum.getOutput());
-
-        }
-
-        Set<Sum3> uniqueSums = new HashSet<>(sums);
-        Assert.assertTrue("Insufficient variation on Sum3s", uniqueSums.size()>1);
-
-        Assert.assertTrue("Insufficient variation on T1", uniqueInput1.size()>1);
-        Assert.assertTrue("Insufficient variation on T2", uniqueInput2.size()>1);
-        Assert.assertTrue("Insufficient variation on T3", uniqueInput3.size()>1);
-        Assert.assertTrue("Insufficient variation on output", uniqueOutput.size()>1);
+        List<SumRep> sumReps = sums.stream().map(sum -> new Sum3RepAdapter(sum)).collect(Collectors.toList());
+        assertCompliantAndDiverseSumReps(size, min, max, false, sumReps);
     }
 
     public static void assertCompliantAndDiverseSumReps(int size, int min, int max, boolean checkOperatorDiversity, List<SumRep> sums) {
