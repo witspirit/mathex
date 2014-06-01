7638b5d2bc3e9d3a0d20f1b1287c21ebb662e374
witspirit <witspirit@gmail.com>
2014-06-01 21:40:07 +0200

    Added statistics counting

--- web-ui/src/main/webapp/resources/js/main.js
+++ web-ui/src/main/webapp/resources/js/main.js
@@ -1,3 +1,39 @@
+var stats = function() {
+    var total = 0;
+    var correct = 0;
+    var mistakes = 0;
+
+    var reset = function() {
+        total = 0;
+        correct = 0;
+        mistakes = 0;
+    };
+
+    var trackCorrect = function() {
+        total++;
+        correct++;
+    };
+
+    var trackMistake = function() {
+        total++;
+        mistakes++;
+    };
+
+    var computeScore = function() {
+        return (correct / total) * 100;
+    };
+
+    return {
+        getTotal : function() { return total; },
+        getCorrect : function() { return correct; },
+        getMistakes : function() { return mistakes; },
+        getScore : computeScore,
+        trackCorrect : trackCorrect,
+        trackMistake : trackMistake,
+        reset : reset
+    };
+}();
+
 var main = function() {
 
     var exercise = mathexgen.next();
@@ -8,7 +44,14 @@ var main = function() {
         $("#max").val(mathexgen.config.max);
         $("#sum2").prop('checked', mathexgen.config.sum2);
         $("#sum3").prop('checked', mathexgen.config.sum3);
-    }
+    };
+
+    var showStats = function() {
+        $("#total").text(stats.getTotal());
+        $("#correct").text(stats.getCorrect());
+        $("#mistakes").text(stats.getMistakes());
+        $("#score").text(stats.getScore());
+    };
 
     var updateConfig = function() {
         var min = $("#min").val()-0;
@@ -16,16 +59,12 @@ var main = function() {
         var sum2 = $("#sum2").prop("checked");
         var sum3 = $("#sum3").prop("checked");
 
-//        console.log("min = "+min+" ("+typeof min+")");
-//        console.log("max = "+max+" ("+typeof max+")");
-//        console.log("sum2 = "+sum2+" ("+typeof sum2+")");
-//        console.log("sum3 = "+sum3+" ("+typeof sum3+")");
-
         mathexgen.config.min = min;
         mathexgen.config.max = max;
         mathexgen.config.sum2 = sum2;
         mathexgen.config.sum3 = sum3;
-    }
+    };
+
 
     $(document).ready(function() {
         showConfig();
@@ -36,14 +75,17 @@ var main = function() {
             var response = $("#response").val()-0; // Convert to number
 
             if (response === exercise.solution) {
+                stats.trackCorrect();
                 exercise = mathexgen.next();
                 visuals.updateExercise(exercise.exercise);
             } else {
                 // There was a mistake...
+                stats.trackMistake();
                 visuals.markError();
             }
 
             showConfig();
+            showStats();
             $("#response").val("");
         });
     });
